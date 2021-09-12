db336a1919e5b2596a991ae2ea685e47e1db2df6
ysnr8811 <y.f.arctos9@gmail.com>
2021-09-12 17:58:09 +0900

    新しいビューを表示するように修正

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index');
+Route::get('hello', function(){
+    return view('hello.index');
+}
+);
